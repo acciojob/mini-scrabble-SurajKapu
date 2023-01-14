1f462936920eda03ee2c95469a876d8b41d1d3fd
SurajKapu <98162452+SurajKapu@users.noreply.github.com>
2023-01-14 09:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let input = document.getElementById("evaluatedText")
+let letter_count = document.getElementById("letterCount")
+  
+input.onkeyup = display_count
+  
+  function display_count() {
+    letter_count.innerText = input.value.length
+  }
